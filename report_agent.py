4494d12ce1443258f121d8817e6fe21d58409e4c
Radha Pandey <pandeymuskan072@gmail.com>
2026-07-22 00:13:41 +0530

    Update report_agent.py

--- report_agent.py
+++ report_agent.py
@@ -19,53 +19,34 @@ class ReportAgent:
     # -----------------------------------------------------
 
     def generate_report(self,prediction,feature_importance):
-
-    feature_text=""
-
-    for _,r in feature_importance.head(8).iterrows():
-
-        feature_text+=f"- {r['Feature']} : {round(float(r['Contribution']),2)}%\n"
-
-    prompt=f"""
-You are a Hydrogen Sustainability Expert.
-
-Generate a professional report.
-
-Location:
-{prediction['Location']}
-
-Latitude:
-{prediction['Latitude']}
-
-Longitude:
-{prediction['Longitude']}
-
-Predicted Hydrogen:
-{prediction['Hydrogen_Output']} kg/day
-
-Estimated CO₂:
-{prediction['CO2_Emission']} kg CO₂-eq/kg H₂
-
-Important Features:
-
-{feature_text}
-
-Generate
-
-1 Executive Summary
-
-2 Technical Analysis
-
-3 Environmental Assessment
-
-4 Explainable AI
-
-5 Sustainability Score
-
-6 Recommendations
-
-7 Conclusion
-"""
+        feature_text=""
+        for _,r in feature_importance.head(8).iterrows():
+            feature_text+=f"- {r['Feature']} : {round(float(r['Contribution']),2)}%\n"
+            prompt=f"""
+            
+            You are a Hydrogen Sustainability Expert.
+            Generate a professional report.
+            Location:
+            {prediction['Location']}
+            Latitude:
+            {prediction['Latitude']}
+            Longitude:
+            {prediction['Longitude']}
+            Predicted Hydrogen:
+            {prediction['Hydrogen_Output']} kg/day
+            Estimated CO₂:
+            {prediction['CO2_Emission']} kg CO₂-eq/kg H₂
+            Important Features:
+            {feature_text}
+            Generate
+            1 Executive Summary
+            2 Technical Analysis
+            3 Environmental Assessment
+            4 Explainable AI
+            5 Sustainability Score
+            6 Recommendations
+            7 Conclusion
+            """
 
     try:
 
