d750cff9e263935db849daff38c588d93f981939
Radha Pandey <pandeymuskan072@gmail.com>
2026-07-22 21:09:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,8 +20,7 @@ load_dotenv(PROJECT_ROOT / ".env")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 # Latest stable Gemini model
-GEMINI_MODEL = "gemini-2.0-flash"
-
+GEMINI_MODEL = "gemini-2.5-flash-lite"
 # ---------------------------------------------------------
 # DATASETS
 # ---------------------------------------------------------
